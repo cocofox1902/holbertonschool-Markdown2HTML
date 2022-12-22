8f34289abce8da7f47a440e0f005de23435eca42
cocofox1902 <colas.renard1902@gmail.com>
2022-12-22 15:31:23 +0100

    add chnage

--- markdown2html.py
+++ markdown2html.py
@@ -6,7 +6,7 @@ import hashlib
 import sys
 import os
 
-if __name__ == "__main__":
+if __name__ == "__main__"
     if len(sys.argv) < 3:
         sys.stderr.write("Usage: ./markdown2html.py README.md README.html\n")
         exit(1)
