a2436dc4b0d0ba3d5f492a38175968154837b2d7
cocofox1902 <colas.renard1902@gmail.com>
2022-12-22 14:52:11 +0100

    test

--- markdown2html.py
+++ markdown2html.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-""" Script that transforme README.md en page HTML """
+""" Write a script markdown2html.py that takes an argument 2 strings:
+
+First argument is the name of the Markdown file
+Second argument is the output file name """
 
 import re
 import hashlib
@@ -14,29 +17,29 @@ if __name__ == "__main__":
         sys.stderr.write("Missing " + sys.argv[1] + "\n")
         exit(1)
 
-    with open(sys.argv[1]) as markdown_file:
-        with open(sys.argv[2], 'w') as html_file:
+    with open(sys.argv[1]) as r:
+        with open(sys.argv[2], 'w') as w:
             change_status = False
             ordered_status = False
             paragraph = False
-            for line in markdown_file:
+            for line in r:
                 line = line.replace('**', '<b>', 1)
                 line = line.replace('**', '</b>', 1)
                 line = line.replace('__', '<em>', 1)
                 line = line.replace('__', '</em>', 1)
 
-                md5_tags = re.findall(r'\[\[.+?\]\]', line)
-                md5_texts = re.findall(r'\[\[(.+?)\]\]', line)
-                if md5_tags:
-                    line = line.replace(md5_tags[0], hashlib.md5(
-                        md5_texts[0].encode()).hexdigest())
+                md5 = re.findall(r'\[\[.+?\]\]', line)
+                md5_inside = re.findall(r'\[\[(.+?)\]\]', line)
+                if md5:
+                    line = line.replace(md5[0], hashlib.md5(
+                        md5_inside[0].encode()).hexdigest())
 
-                delete_c_tags = re.findall(r'\(\(.+?\)\)', line)
-                delete_c_texts = re.findall(r'\(\((.+?)\)\)', line)
-                if delete_c_tags:
-                    delete_c_texts = ''.join(
-                        c for c in delete_c_texts[0] if c not in 'Cc')
-                    line = line.replace(delete_c_tags[0], delete_c_texts)
+                delete_c = re.findall(r'\(\(.+?\)\)', line)
+                remove_c_inside = re.findall(r'\(\((.+?)\)\)', line)
+                if delete_c:
+                    remove_c_inside = ''.join(
+                        c for c in remove_c_inside[0] if c not in 'Cc')
+                    line = line.replace(delete_c[0], remove_c_inside)
 
                 length = len(line)
                 headings = line.lstrip('#')
@@ -50,43 +53,41 @@ if __name__ == "__main__":
                     line = '<h{}>'.format(
                         heading_count) + headings.strip() + '</h{}>\n'.format(
                         heading_count)
-                
-                # * line = f'<h{heading_count}>{headings.strip()}</h{heading_count}>\n'
 
                 if unordered_count:
                     if not change_status:
-                        html_file.write('<ul>\n')
+                        w.write('<ul>\n')
                         change_status = True
                     line = '<li>' + unordered.strip() + '</li>\n'
                 if change_status and not unordered_count:
-                    html_file.write('</ul>\n')
+                    w.write('</ul>\n')
                     change_status = False
 
                 if ordered_count:
                     if not ordered_status:
-                        html_file.write('<ol>\n')
+                        w.write('<ol>\n')
                         ordered_status = True
                     line = '<li>' + ordered.strip() + '</li>\n'
                 if ordered_status and not ordered_count:
-                    html_file.write('</ol>\n')
+                    w.write('</ol>\n')
                     ordered_status = False
 
                 if not (heading_count or change_status or ordered_status):
                     if not paragraph and length > 1:
-                        html_file.write('<p>\n')
+                        w.write('<p>\n')
                         paragraph = True
                     elif length > 1:
-                        html_file.write('<br/>\n')
+                        w.write('<br/>\n')
                     elif paragraph:
-                        html_file.write('</p>\n')
+                        w.write('</p>\n')
                         paragraph = False
 
                 if length > 1:
-                    html_file.write(line)
+                    w.write(line)
 
             if ordered_status:
-                html_file.write('</ol>\n')
+                w.write('</ol>\n')
             if paragraph:
-                html_file.write('</p>\n')
+                w.write('</p>\n')
 
     exit(0)
